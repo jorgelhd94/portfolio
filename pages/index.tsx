50392c0de89caf53b3fa29bf1247f3c4e1d669b6
Jorge Luis Hernández Dueñas <jorgelhd94@gmail.com>
2022-11-04 14:13:05 -0400

    add title section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from "next";
+import About from "../src/components/sections/About/About";
 import Layout from "../src/components/templates/Layout/Layout";
 
 const Home: NextPage = () => {
   return (
     <Layout>
-      
+      <About />
     </Layout>
   );
 };
