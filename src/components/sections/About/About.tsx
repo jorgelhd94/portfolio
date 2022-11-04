75e1cc0855ab011e1629119e8a62db60511cdfad
Jorge Luis Hernández Dueñas <jorgelhd94@gmail.com>
2022-11-04 14:20:16 -0400

    add text styles

--- src/components/sections/About/About.tsx
+++ src/components/sections/About/About.tsx
@@ -4,6 +4,7 @@ const About = () => {
   return (
     <>
       <h2 className="title-section after:left-52">About Me</h2>
+      
       <div className="lg:grid grid-cols-12 md:gap-10 pt-4 md:pt-[30px] items-center hidden">
         <div className="col-span-12 space-y-2.5">
           <div className="lg:mr-16">
