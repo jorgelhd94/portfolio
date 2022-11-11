ea846b9ba2cd9f8c0127fff22e39102343b41b41
Jorge Luis Hernandez Dueñas <jorgelhd94@gmail.com>
2022-11-11 11:00:38 -0500

    work in work card

--- src/components/sections/Works/Works.tsx
+++ src/components/sections/Works/Works.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
-import Image from 'next/image';
+import WorkCard from '../../molecules/Cards/WorkCard/WorkCard';
+import TaskApp from '../../../../public/tasklist.jpg';
+import QrApp from '../../../../public/niceqr.jpg';
 
 const Works = () => {
   return (
@@ -15,27 +17,9 @@ const Works = () => {
         <h4 className="text-4xl dark:text-white font-medium">Works</h4>
       </div>
 
-      <div className="flex flex-row flex-wrap">
-        <a
-          href="#"
-          className="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
-        >
-          <Image
-            className="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-            src="/docs/images/blog/image-4.jpg"
-            layout='fill'
-            alt=""
-          />
-          <div className="flex flex-row justify-between p-4 leading-normal">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
-            </h5>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-          </div>
-        </a>
+      <div className="flex justify-between">
+        <WorkCard image={QrApp} title="Nice QR" />
+        <WorkCard image={TaskApp} title="Simple Task List" />
       </div>
     </div>
   );
