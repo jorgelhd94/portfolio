465aad443980ea0ffb42568175b4c0e1ee12740d
Jorge Luis Hernández Dueñas <jorgelhd94@gmail.com>
2022-11-11 15:08:26 -0800

    update config

--- next.config.js
+++ next.config.js
@@ -1,23 +1,24 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-let assetPrefix = ''
-let basePath = '/'
+let repo = undefined;
+let assetPrefix = '';
+let basePath = '/';
 
 if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
 
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
 }
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: { unoptimized: true },
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+  assetPrefix: repo ? assetPrefix : undefined,
+  basePath: repo ? basePath : undefined,
 };
 
 module.exports = nextConfig;
